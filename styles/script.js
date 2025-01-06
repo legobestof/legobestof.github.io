d4dff571589146fff312f0b034dff780d68bb1b2
nathan_pierreuse <nathan_pierreuse@ens.univ-artois.fr>
2025-01-06 15:16:45 +0100

    ajout des projets

--- styles/script.js
+++ styles/script.js
@@ -30,23 +30,24 @@ function openPopup(projectId) {
   if (projectId === "project1") {
     title.textContent = "Projet 1";
     description.textContent =
-      "Description détaillée du projet 1. Ce projet est un exemple de travail avec des technologies comme HTML, CSS et JavaScript.";
-    link.href = "https://github.com/ton-repo1"; // Remplacer par le lien GitHub réel
+      "Marathon de 2 jours au sein de l'IUT de Lens avec comme sujet un site qui fait office de carnet de voyage, en collaboration avec le département MMI qui s'est occupé de la partie visuel. Le départememnt informatique lui s'est occupé des fonctionalité du site en utilisant les languages php et html et le framework laravel.";
+    link.href = "https://github.com/legobestof/var_dump";
   } else if (projectId === "project2") {
     title.textContent = "Projet 2";
     description.textContent =
-      "Description détaillée du projet 2. Ce projet est un exemple de travail avec des frameworks comme React.";
-    link.href = "https://github.com/ton-repo2"; // Remplacer par le lien GitHub réel
+      "LensJudge est un juge automatique pour compétition de programmation qui prend en compte plusieurs cas, avec par exemple la case mais aussi bien sûr plusieurs language de programmation.";
+    link.href = "https://github.com/legobestof/LENSJudge";
   } else if (projectId === "project3") {
     title.textContent = "Projet 3";
     description.textContent =
-      "Description détaillée du projet 3. Ce projet utilise des bases de données et un back-end en Node.js.";
-    link.href = "https://github.com/ton-repo3"; // Remplacer par le lien GitHub réel
+      "Jeux Bomberman créer en java avec plusieurs niveau, différents type d'ennemis et différentes bombe. Utilisation de javaFX pour gérer la fenêtre du jeu.";
+    link.href =
+      "https://github.com/legobestof/Bomberman_GroupeC5_Alexandre_Lucas_Nathan";
   } else if (projectId === "project4") {
     title.textContent = "Projet 4";
     description.textContent =
-      "Description détaillée du projet 4. Ce projet implique la création d'une application mobile avec Flutter.";
-    link.href = "https://github.com/ton-repo4"; // Remplacer par le lien GitHub réel
+      "Jeux du juste prix utilisant une API pour récupérer des produits Amazon et faire deviner leur prix. Utilisant python et flask mais aussi html css pour l'interface utilisateur.";
+    link.href = "https://github.com/legobestof/MSI-Team-Malphite-WhatsApp";
   }
 
   popup.style.display = "flex"; // Afficher la pop-up
